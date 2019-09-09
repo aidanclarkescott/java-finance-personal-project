d3869b9ff6d322b41689660c6ca59975ed787f14
Aidan CS <aclarkescott@shaw.ca>
2019-09-09 15:26:59 -0700

    Created methods and print statements.

---
+++ src/test/ui/Main.java
@@ -0,0 +1,20 @@
+package ui;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printStocks();
+        printBudget();
+
+    }
+
+    public static void printStocks() {
+        System.out.println("Stocks: \nAAPL \nAMZN \netc. \n");
+
+    }
+
+    public static void printBudget () {
+        System.out.println("Budget: \nGroceries: x \nEntertainment: y \nClothes: z \netc. \n");
+    }
+}
+
